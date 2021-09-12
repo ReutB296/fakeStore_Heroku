bbe12e84f1d263d9dc95b91990787df133a91527
reut <justreut1@gmail.com>
2021-09-12 19:22:43 +0300

    first commit

---
+++ server/server.mjs
@@ -0,0 +1,28 @@
+import express from 'express';
+import logger from 'morgan';
+
+import 'express-async-errors';
+
+import { productsRouter } from './src/products.routes.mjs';
+import { usersRouter } from './src/users.routes.mjs';
+import { categoriesRouter } from './src/categories.routes.mjs';
+
+import './src/db/connect.mjs';
+
+export const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+
+app.use('/api/products', productsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/categories', categoriesRouter);
+
+app.use(express.static('../client/build/'));
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+
+console.log("Server is listening on http://localhost:" + port);
---
+++ server/src/db/connect.mjs
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017//fakestore';
+
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
